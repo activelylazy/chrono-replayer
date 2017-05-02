fd87f1de8f9fbbfcbe14900754bac66590c29977
David Green <dave@activelylazy.co.uk>
2017-05-02 13:01:26 +0100

    Serve up cur.js

--- src/index.js
+++ src/index.js
@@ -5,11 +5,18 @@ const app = express();
 const basePath = '../spike/timing-spike/sessions/2017-04-16_1545';
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send(`Timing replayer for ${basePath}`);
 });
 
 app.get('/all.js', (req, res) => {
   const data = fs.readFileSync(`${basePath}/all.js`);
+  res.setHeader('Content-Type', 'application/javascript');
+  res.send(data);
+});
+
+app.get('/cur.js', (req, res) => {
+  const timestamp = Object.keys(req.query)[0];
+  const data = fs.readFileSync(`${basePath}/cur.js_${timestamp}`);
   res.setHeader('Content-Type', 'application/javascript');
   res.send(data);
 });
