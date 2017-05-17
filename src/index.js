9721d364b7268c4cb15761d14b18e7743ec9914c
David Green <dave@activelylazy.co.uk>
2017-05-17 08:01:11 +0100

    Fix path

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 
 const app = express();
-const basePath = '../sessions/2017-Bahrain-RACE-Part1';
+const basePath = '../sessions/2017-R03-Bahrain-RACE-Part1';
 
 app.get('/', (req, res) => {
   res.send(`Timing replayer for ${basePath}`);
